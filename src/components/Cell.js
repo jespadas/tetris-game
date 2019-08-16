f4fa092a2d3158cc384fe445b991302fc188da85
Julio Espadas <julio.espadas@gmail.com>
2019-08-16 14:47:46 +0200

    useGameStatus and Reac.memo added

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -4,7 +4,7 @@ import { StyledCell } from "./styles/StyleCell";
 import { TETROMINOS } from "../tetrominos";
 
 const Cell = ({ type }) => (
-  <StyledCell type={type} color={TETROMINOS[type].color}/>
+  <StyledCell type={type} color={TETROMINOS[type].color}>{console.log("rerender")}</StyledCell>
 );
 
-export default Cell;
+export default React.memo(Cell);
